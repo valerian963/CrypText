44939b889ab3eab7f3eb9ffc4448ab6bb186807c
valerian963 <valeriasantos963@gmail.com>
2025-09-21 21:27:24 -0300

    added accept-friend request

--- backend/server.js
+++ backend/server.js
@@ -196,7 +196,7 @@ io.on('connection', (socket) => {
 
   // Evento de solicitar amizade
   socket.on('friend-request', async (user_name1Encrypted, user_name2Encrypted, blowfish_keyEncrypted, signature , callback) => {
-    console.log("//Login------------------------------------\n")
+    console.log("//Friend request------------------------------------\n")
     try {
 
       const blowfish_key = rsa.decrypt(caPrivateKey, blowfish_keyEncrypted);
@@ -261,6 +261,68 @@ io.on('connection', (socket) => {
       callback({ success: false, message: 'Erro ao enviar solicitação de amizade: ', error });
       console.log('Erro ao enviar solicitação de amizade: ', error);
     }
+  });
+
+
+  // Evento de aceitar solicitação
+  socket.on('accept-friend', async (user_name1Encrypted, user_name2Encrypted, blowfish_keyEncrypted, signature , callback) => {
+    console.log("//Accept friend------------------------------------\n")
+    try {
+
+      const blowfish_key = rsa.decrypt(caPrivateKey, blowfish_keyEncrypted);
+      const user_name1 = blowfish.decrypt(user_name1Encrypted, blowfish_key, {cipherMode: 0, outputType: 0});
+      const user_name2 = blowfish.decrypt(user_name2Encrypted, blowfish_key, {cipherMode: 0, outputType: 0});
+      
+      const pubKeyUser = await getUserPublicKey(user_name1);
+      const pubKeyUser_sender = await getUserPublicKey(user_name2);
+      
+      console.log('Dados recebidos criptografados: ');
+      console.log({sender_value: user_name1Encrypted, receiver_value: user_name2Encrypted});
+      console.log('\nDados recebidos descriptografados: ');
+      console.log({sender_value: user_name1, receiver_value: user_name2});
+
+      console.log(`Usuário ${user_name2} aceitou pedido de amizade de ${user_name1}`);
+      // Verifica se já existe uma solicitação pendente ou aceita
+      const existingRequest = await pool.query(
+        'SELECT * FROM users_friends WHERE ((friend1 = $1 AND friend2 = $2) OR (friend1 = $2 AND friend2 = $1)) AND friendship = true',
+        [user_name1, user_name2]
+      );
+      if (existingRequest.rowCount > 0) {
+        callback({ success: false, message: 'Solicitação já enviada' });
+        return;
+      }
+
+      // Alterar estado da solicitacao de amizade no banco de dados para true (aceito)
+      await pool.query(
+        'UPDATE users_friends SET friendship = $3 WHERE (friend1 = $1 AND friend2 = $2) OR (friend1 = $2 AND friend2 = $1)',
+      [user_name1, user_name2,true]
+      );
+
+      callback({ success: true, message: 'Amizade aceita' });
+
+      if (onlineUsers[user_name1]) {
+        // Se o destinatário está online, envie o aceite de amizade diretamente
+        console.log(`Notificando usuário ${user_name1}: do aceite de amizade de ${user_name2}`);
+        const recipientSocketId = onlineUsers[user_name1]
+        io.to(recipientSocketId).emit('accepted-friendship', 
+          {user_name: blowfish.encrypt(user_name2, caPrivateKey,{cipherMode: 0, outputType: 0}),
+          certificate: blowfish.encrypt(pubKeyUser, caPrivateKey,{cipherMode: 0, outputType: 0})
+          });
+        }
+        else{
+        console.log(`Aceite do usuário ${user_name2} para amizade de ${user_name1} armazenado no banco de dados`);
+        await pool.query(
+          `
+          INSERT INTO answered_requests (friend1, friend2, publicKey_friend2, accepted)
+          VALUES ($1, $2, $3, $4);
+          `,
+          [user_name1, user_name2, publicKey_friend2, true]
+        );
+        }
+      } catch (error) {
+        console.error('Erro ao aceitar solicitação de amizade:', error);
+        callback({ success: false, message: 'Erro ao aceitar solicitação de amizade' });
+      }
   });
 });
 
